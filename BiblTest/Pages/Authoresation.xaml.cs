faead955651d34505ae06ab626b5d3df2548d030
burundur9 <misbakhovdanis2005@gmail.com>
2025-02-26 10:39:00 +0300

    Добавьте файлы проекта.

---
+++ BiblTest/Pages/Authoresation.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BiblTest.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authoresation.xaml
+    /// </summary>
+    public partial class Authoresation : Page
+    {
+        public Authoresation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVhod_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MenuEmployeePage());
+        }
+
+        private void btnVhodChit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AuthorisationPage());
+        }
+    }
+}
---
+++ BiblTest/Pages/MenuEmployeePage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BiblTest.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuEmployeePage.xaml
+    /// </summary>
+    public partial class MenuEmployeePage : Page
+    {
+        public MenuEmployeePage()
+        {
+            InitializeComponent();
+        }
+
+        private void readerBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bookBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void reportBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
